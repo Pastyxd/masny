96087852d8d5b14e6362b4ec4372ec1903213c99
ondrej masny <masny@skolabaltaci.cz>
2024-10-08 07:38:12 +0200

    feat: add db generated files
    ¨

--- eshop_biocolors/database/migrations/2024_10_08_053323_create_products_table.php
+++ eshop_biocolors/database/migrations/2024_10_08_053323_create_products_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-        });
+            $table->string('name');
+            $table->string('color');
+            $table->string('size');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('products');
